bad7e0e89cce5e0fdb9a5bfa846a5f5ada99a03d
kawther boukamcha <boukamchakawther@gmail.com>
2022-05-19 11:07:47 +0100

    jsx

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+
+import ImageInSrc from  "./imageInSrc.jpg"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+  <div style={{ border: "solid 1px black", maxWidth: "100vw" }}>
+    <h1 className="title red">Kawther BOUKAMCHA</h1>
+    <br />
+    <img src={ImageInSrc} alt ='imageInSrc'  />
+    <br />
+    <img src="imageInPublic.jpg" width={800} height={500} />
+  </div>
+  <video width={320} height={240} controls="">
+    <source src="myVideo.mp4" type="video/mp4" />
+  </video>
+</>
+
   );
 }
 
